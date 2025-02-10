3b4259274efe763316b47564c5de0e351fed0fec
husainb0112 <husainb@nycstudents.net>
2025-02-10 14:31:14 +0000

    v1

---
+++ WeatherData.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class WeatherData {
+    
+    private ArrayList<Double> temperatures;
+
+    public void cleanData(double lower, double upper) {
+        
+    }
+
+    public int longestHeatWave(double threshhold) {
+        return 0;
+    }
+
+}
