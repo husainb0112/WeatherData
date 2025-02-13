2f5a98f7d85c970218e790b58f785f0eb7c3e443
husainb0112 <husainb@nycstudents.net>
2025-02-13 14:31:20 +0000

    v2

--- WeatherData.java
+++ WeatherData.java
@@ -4,7 +4,11 @@ public class WeatherData {
     private ArrayList<Double> temperatures;
 
     public void cleanData(double lower, double upper) {
-        
+        for (double temp : temperatures) {
+            if (temp <= lower || temp >= upper) { 
+                temperatures.remove(temp); 
+            }
+        }
     }
 
     public int longestHeatWave(double threshhold) {
